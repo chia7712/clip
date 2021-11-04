948c3d6dc3536767558d00d63d80d23d46c5d07a
Chia-Ping Tsai <chia7712@gmail.com>
2021-11-04 15:28:37 +0800

    don't delete namespace too quick

--- yunikorn/restart_yunikorn.sh
+++ yunikorn/restart_yunikorn.sh
@@ -3,11 +3,6 @@
 # get path
 BASEDIR=$(dirname $0)
 
-# prepare namespace
-namespace="yunikorn"
-kubectl delete namespace $namespace \
-; kubectl create namespace $namespace
-
 # pre-pull yunikorn image
 docker pull chia7712/yunikorn:scheduler-latest
 docker pull chia7712/yunikorn:web-latest
@@ -18,6 +13,8 @@ kubectl delete deployment/yunikorn-scheduler -n $namespace \
 ; kubectl delete clusterrolebinding yunikorn-rbac -n $namespace \
 ; kubectl delete serviceaccount yunikorn-admin -n $namespace \
 ; kubectl delete configmap yunikorn-configs -n $namespace \
+; kubectl delete namespace $namespace \
+; kubectl create namespace $namespace \
 ; kubectl create -f "$BASEDIR/yunikorn-rbac.yaml" \
 && kubectl create configmap yunikorn-configs --from-file=queues.yaml="$BASEDIR/queues.yaml" -n $namespace \
 && kubectl create -f "$BASEDIR/scheduler.yaml" -n $namespace
