b4b2c7f9241e7af4d6f2609ffa6c4c64e50e6bca
Chia-Ping Tsai <chia7712@gmail.com>
2022-01-28 23:40:13 +0800

    Use flannel to replace calico

--- k8s/start_k8s.sh
+++ k8s/start_k8s.sh
@@ -5,13 +5,12 @@ BASEDIR=$(dirname $0)
 
 sudo swapoff -a
 sudo rm -rf /etc/cni/net.d
-sudo kubeadm init --control-plane-endpoint=k8s00 --pod-network-cidr=192.168.100.0/24
+sudo kubeadm init --control-plane-endpoint=k8s00 --pod-network-cidr=10.244.0.0/16
 rm -rf $HOME/.kube
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 sleep 5
-kubectl create -f "$BASEDIR/tigera-operator.yaml"
-kubectl create -f "$BASEDIR/custom-resources.yaml"
-sleep 30
+kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+sleep 10
 kubectl taint nodes --all node-role.kubernetes.io/master-
