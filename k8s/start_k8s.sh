eb0650567720eabd5cf5bece6bd70076da774d99
Chia-Ping Tsai <chia7712@gmail.com>
2022-02-07 17:32:58 +0800

    refactor start_k8s.sh

--- k8s/start_k8s.sh
+++ k8s/start_k8s.sh
@@ -1,16 +1,60 @@
 #!/bin/bash
 
-# get path
-BASEDIR=$(dirname $0)
+# ===================================[functions]===================================
 
-sudo swapoff -a
-sudo rm -rf /etc/cni/net.d
-sudo kubeadm init --control-plane-endpoint=k8s00 --pod-network-cidr=10.244.0.0/16
-rm -rf $HOME/.kube
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-sleep 5
-kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
-sleep 10
-kubectl taint nodes --all node-role.kubernetes.io/master-
+function showHelp() {
+  echo "Usage: [ENV] start_k8s.sh"
+  echo "Arguments: "
+  echo "    --single   true if you want to build single-machine Kubernetes cluster"
+}
+
+function cleanup() {
+  sudo swapoff -a
+  sudo rm -rf /etc/cni/net.d
+  rm -rf "$HOME"/.kube
+}
+
+function initK8s() {
+  sudo kubeadm init --control-plane-endpoint=k8s00 --pod-network-cidr=10.244.0.0/16
+  mkdir -p "$HOME"/.kube
+  sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
+  sudo chown $(id -u):$(id -g) "$HOME"/.kube/config
+}
+
+function initNetwork() {
+  kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+}
+
+function untaintControlNode() {
+  kubectl taint nodes --all node-role.kubernetes.io/master-
+}
+
+# ===================================[main]===================================
+
+# true if you want to build single-machine Kubernetes cluster
+single="false"
+while [[ $# -gt 0 ]]; do
+  case $1 in
+  --single)
+    single="$2"
+    shift
+    shift
+    ;;
+  --help)
+    showHelp
+    exit 0
+    ;;
+  *)
+    echo "Unknown option $1"
+    exit 1
+    ;;
+  esac
+done
+
+cleanup
+initK8s
+initNetwork
+
+if [[ "$single" == "true" ]]; then
+  untaintControlNode
+fi
