c174ff1f1a6f295f29c8f497adbc7e07446d4d32
Chia-Ping Tsai <chia7712@gmail.com>
2022-02-09 22:47:56 +0800

    generate yunikorn yaml from script

--- yunikorn/restart_yunikorn.sh
+++ yunikorn/restart_yunikorn.sh
@@ -4,17 +4,19 @@ declare -r SCRIPT_FOLDER=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null
 declare -r RBAC_FILE="$SCRIPT_FOLDER/yunikorn-rbac.yaml"
 declare -r QUEUES_FILE="$SCRIPT_FOLDER/queues.yaml"
 declare -r SCHEDULER_FILE="$SCRIPT_FOLDER/scheduler.yaml"
-
+declare -r RBAC_NAME="yunikorn-rbac"
+declare -r ADMIN_NAME="yunikorn-admin"
+declare -r CONFIG_NAME="yunikorn-configs"
 
 # ===================================[functions]===================================
 
 function showHelp() {
   echo "Usage: [ENV] restart_yunikorn.sh"
   echo "Arguments: "
-  echo "    --namespace   k8s namespace"
-  echo "    --account     k8s account"
+  echo "    --image       docker image of yunikorn scheduler"
+  echo "    --namespace   k8s namespace to deploy yunikorn scheduler"
+  echo "    --account     k8s account for spark application"
 }
-
 
 function requireNonEmpty() {
   local var=$1
@@ -25,10 +27,123 @@ function requireNonEmpty() {
   fi
 }
 
+function generateScheduler() {
+  local image=$1
+  echo "
+# this file is generated automatically
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  labels:
+    app: yunikorn
+  name: yunikorn-scheduler
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: yunikorn
+  template:
+    metadata:
+      labels:
+        app: yunikorn
+        component: yunikorn-scheduler
+      name: yunikorn-scheduler
+    spec:
+      hostNetwork: true
+      serviceAccountName: yunikorn-admin
+      containers:
+        - name: yunikorn-scheduler-k8s
+          image: $image
+          env:
+            - name: LOG_ENCODING
+              value: console
+            - name: LOG_LEVEL
+              value: '-1'
+            - name: CLUSTER_ID
+              value: myCluster
+            - name: CLUSTER_VERSION
+              value: latest
+          resources:
+            requests:
+              cpu: 200m
+              memory: 1Gi
+            limits:
+              cpu: 4
+              memory: 2Gi
+          imagePullPolicy: Always
+          ports:
+            - containerPort: 9080
+            - containerPort: 9090
+          volumeMounts:
+            - name: config-volume
+              mountPath: /etc/yunikorn/
+      volumes:
+        - name: config-volume
+          configMap:
+            name: $CONFIG_NAME
+" >"$SCHEDULER_FILE"
+}
+
+function generateRbac() {
+  local namespace=$1
+
+  echo "
+# this file is generated automatically
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: $ADMIN_NAME
+  namespace: $namespace
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: $RBAC_NAME
+subjects:
+  - kind: ServiceAccount
+    name: $ADMIN_NAME
+    namespace: $namespace
+roleRef:
+  kind: ClusterRole
+  name: cluster-admin
+  apiGroup: rbac.authorization.k8s.io
+
+" >"$RBAC_FILE"
+}
+
+function cleanup() {
+  local namespace=$1
+  kubectl delete deployment/yunikorn-scheduler -n "$namespace" \
+    ;
+  kubectl delete clusterrolebinding $RBAC_NAME -n "$namespace" \
+    ;
+  kubectl delete serviceaccount $ADMIN_NAME -n "$namespace" \
+    ;
+  kubectl delete configmap $CONFIG_NAME -n "$namespace"
+}
+
+function setup() {
+  local namespace=$1
+  kubectl create namespace "$namespace" \
+    ;
+  kubectl create -f "$RBAC_FILE" &&
+    kubectl create configmap $CONFIG_NAME --from-file=queues.yaml="$QUEUES_FILE" -n "$namespace" &&
+    kubectl create -f "$SCHEDULER_FILE" -n "$namespace"
+}
+
+function setupSpark() {
+  local namespace=$1
+  local account=$2
+  kubectl delete rolebinding spark-on-yunikorn -n "$namespace" \
+    ;
+  kubectl create rolebinding spark-on-yunikorn --clusterrole=edit --serviceaccount=$namespace:$account -n "$namespace"
+}
+
 # ===================================[main]===================================
 
-namespace="yunikorn"
-account="default"
+image="ghcr.io/chia7712/yunikorn/scheduler:latest"
+namespace="spark"
+account=""
 while [[ $# -gt 0 ]]; do
   case $1 in
   --namespace)
@@ -38,6 +153,11 @@ while [[ $# -gt 0 ]]; do
     ;;
   --account)
     account="$2"
+    shift
+    shift
+    ;;
+  --image)
+    image="$2"
     shift
     shift
     ;;
@@ -52,18 +172,15 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-requireNonEmpty "$account" "account can not be empty"
 requireNonEmpty "$namespace" "namespace can not be empty"
+requireNonEmpty "$image" "image can not be empty"
 
-kubectl delete deployment/yunikorn-scheduler -n $namespace \
-; kubectl delete clusterrolebinding yunikorn-rbac -n $namespace \
-; kubectl delete serviceaccount yunikorn-admin -n $namespace \
-; kubectl delete configmap yunikorn-configs -n $namespace \
-; kubectl create namespace $namespace \
-; kubectl create -f "$RBAC_FILE" \
-&& kubectl create configmap yunikorn-configs --from-file=queues.yaml="$QUEUES_FILE" -n $namespace \
-&& kubectl create -f "$SCHEDULER_FILE" -n $namespace
+generateRbac "$namespace"
+generateScheduler "$image"
+cleanup "$namespace"
+setup "$namespace"
 
 # prepare rbac for spark
-kubectl delete rolebinding spark-on-yunikorn -n $namespace \
-; kubectl create rolebinding spark-on-yunikorn --clusterrole=edit --serviceaccount=$namespace:$account -n $namespace
+if [[ "$account" != "" ]]; then
+  setupSpark "$namespace" "$account"
+fi
